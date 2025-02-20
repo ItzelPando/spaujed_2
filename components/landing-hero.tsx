7f81796d6dbbc8816525bad632ee11adde75f63d
Itzel Vazquez Pando <vapig32@gmail.com>
2025-02-20 09:46:03 -0600

    acomodo de la barra de la gaceta

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -15,19 +15,27 @@ export function LandingHero() {
           <div className="absolute inset-0 bg-black/50" />
         </div>
         <div className="container mx-auto px-4 h-full flex items-center justify-center relative z-10">
-          <div className="text-center text-white">
+          <div className="text-center text-white pt-60">
             <h1 className="text-4xl md:text-6xl font-bold mb-6">
               Sindicato de Personal Académico
               <span className="block mt-2">de la UJED</span>
             </h1>
-            <p className="text-xl mb-8 max-w-2xl mx-auto">¡Bienvenido! Descarga aquí nuestra gaceta oficial</p>
-            <Button variant="destructive" size="lg">
-              Pasión Sindical
-            </Button>
           </div>
+        </div>
+      </div>
+      <div style={{ backgroundColor: "#DB2723" }} className="w-full py-4">
+        <div className="container mx-auto px-4 flex items-center justify-center text-white bg-red-600">
+          <p className="text-xl mr-4">¡Bienvenido! Descarga aquí nuestra gaceta oficial</p>
+          <Button variant="destructive" size="lg" className="bg-white p-5 text-red-800">
+            Pasión Sindical
+          </Button>
         </div>
       </div>
     </section>
   )
 }
+
+
+
+
 
